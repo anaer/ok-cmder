6cf91c95082e671013569d7ed3ee93981007facc
random <a@b.com>
2024-03-29 10:28:22 +0800

    修改用户配置

--- config/user-config.bat
+++ config/user-config.bat
@@ -2,8 +2,7 @@
 
 :: set GIT_FOR_WIN_ROOT=%CMDER_ROOT%\vendor\git_for_windows
 
-::
-:: if not exist "%HOME%\.vim" (
-::     cd /d "%HOME%"
-::     git clone https://github.com/VundleVim/Vundle.vim.git ./.vim/bundle/Vundle.vim
-:: )
+if not exist "%HOME%\.vim" (
+    cd /d "%HOME%"
+    git clone https://github.com/VundleVim/Vundle.vim.git ./.vim/bundle/Vundle.vim
+)
