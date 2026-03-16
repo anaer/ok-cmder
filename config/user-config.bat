823935dba01bc7b087ee4c77b57cefeb448709db
random <r@n.om>
2026-03-16 10:56:26 +0800

    激活虚拟环境时 设置环境变量

--- config/user-config.bat
+++ config/user-config.bat
@@ -10,4 +10,7 @@ if not exist "%HOME%\.vim" (
 @REM 判断是否存在 .venv 文件夹，如果存在则激活Python虚拟环境
 if exist ".venv" (
     call .venv\Scripts\activate.bat
+
+    @REM 获取当前Python版本并设置环境变量
+    for /f "delims=" %%v in ('".venv\Scripts\python.exe -V"') do set "VENV_PYTHON_VERSION=%%v"
 )
