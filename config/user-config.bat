d3d124cd96aaa56aba585de3585c0e98f5c95649
random <r@n.om>
2026-03-16 10:45:18 +0800

    判断.venv

--- config/user-config.bat
+++ config/user-config.bat
@@ -6,3 +6,8 @@ if not exist "%HOME%\.vim" (
     cd /d "%HOME%"
     git clone https://github.com/VundleVim/Vundle.vim.git ./.vim/bundle/Vundle.vim
 )
+
+@REM 判断是否存在 .venv 文件夹，如果存在则激活Python虚拟环境
+if exist ".venv" (
+    call .venv\Scripts\activate.bat
+)
